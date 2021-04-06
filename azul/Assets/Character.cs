65cd3f935b47a721ae843291ff5115e2584e885e
Agustin Pontura <pontura@gmail.com>
2021-04-06 12:31:10 -0300

    animaciones y escena

--- azul/Assets/Character.cs
+++ azul/Assets/Character.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Data data;
+    Animator anim;
+    public Data[] all;
+    int id;
+    string currentClip;
+
+    [Serializable]
+    public class Data
     {
-        
+        public AnimationClip clip;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
+    {
+        Utils.Shuffle(all);
+        anim = GetComponentInChildren<Animator>();
+        currentClip = "singing";
+    }
+    public void RandomAnim()
+    {
+        data = all[id];
+        //if (currentClip == "singing")
+        //{
+            currentClip = data.clip.name;
+            Invoke(currentClip, 0.1f);
+            anim.CrossFade(currentClip, 0.5f);
+            //anim.SetBool(currentClip, true);
+            id++;
+            if (id > all.Length - 1)
+                id = 0;
+        //}
+        //else
+        //{
+        //    anim.SetBool(currentClip, false);
+        //    currentClip = "singing";
+        //}
+    }
+    public void dancing()
     {
         
     }
+    public void singing()
+    {
+
+    }
+
 }
