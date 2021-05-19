668a9e535aacee1c8b02d86af6479e697af2a365
Agustin Pontura <pontura@gmail.com>
2021-05-19 17:11:58 -0300

    click para arrancar

---
+++ azul/Assets/IntroLoop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroLoop : MonoBehaviour
+{
+    public GameObject signal;
+
+    void Start()
+    {
+        signal.SetActive(false);
+        Restart();
+    }
+    void Restart()
+    {
+        signal.SetActive(false);
+        Invoke("Init", 3);
+    }
+    private void Init()
+    {
+        signal.SetActive(true);
+        Vector2 pos = new Vector2 (Random.Range(-200, 200), Random.Range(-180, 180));
+        signal.transform.localPosition = pos;
+        Invoke("Restart", 2);
+    }
+    public void OnClicked()
+    {
+        CancelInvoke();
+        signal.SetActive(false);
+    }
+}
